a478e9860d19f2339039f3aa3e77cd583f5aca1c
Dorian Hryniewicki <mrdorianh@gmail.com>
2021-07-19 16:50:43 -0400

    Complete Project

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# git_message_generator
-Generate random messages in response to github commits
+# git_message_generator (Mixed Messages)
+Generate random messages in response to github commits.
 
 # PreProduction
 ## Project Objectives:
-    - Build a message generator program using JavaScript
+    - Build a message generator program using JavaScript. The message that it outputs should be made up of at least three different pieces of data.
     - Use Git version control
     - Use command line
     - Develop locally on your computer
+### Topic
+    - Any words related to empathy.
+### Store Message Components
+    - An object containing multiple arrays and a randomized output method returning three points of data.
 ## Prerequisites:
     - JavaScript
     - Git and GitHub
     - Command line
+
+
--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-//
+//console.log("Program Start");
+//sample: Your Jupiter is in Aries
+//sample: You have 'x' earth signs, and 'y' air signs.
+const address = [
+    'You',
+    'Your'
+]
+const planets = [
+    "Mercury",
+    "Earth",
+    "Jupiter",
+    "Uranus",
+    "Mars",
+    "Venus",
+    "Saturn",
+    "Neptune",
+    "Pluto"
+    ]
+const signs = [
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+    "Aquarius",
+    "Pisces"
+]
+
+const getRandNum = (max) => {
+    const num = Math.floor(Math.random() * max);
+    return num;
+}
+
+const randomizer = {
+    output(){
+        let s = "";
+        const startNum = getRandNum(2);
+        s += address[startNum];
+        if(startNum > 0) //Your
+        {
+            s += ' ';
+            s += planets[getRandNum(planets.length)];
+            s+= ' is in ';
+            s += signs[getRandNum(signs.length)];
+            s+= '.'
+        }
+        else{ //You
+            const earthSigns = getRandNum(planets.length);
+            const airSigns = Math.min(planets.length - earthSigns,getRandNum(planets.length));
+            // console.log(earthSigns);
+            // console.log(airSigns);
+            // console.log(earthSigns + airSigns);
+            let signsText = "sign";
+            if(airSigns != 1){
+                signsText += 's';
+            }
+            s+= ` have ${earthSigns} earth signs, and ${airSigns} air ${signsText}.`
+        }
+        console.log(s);
+    }
+}
+
+
+randomizer.output();
+
